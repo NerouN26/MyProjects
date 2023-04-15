a2775305df1e3ff9cfdb8dd2d90e035c24113122
nikita bakulin <122795813+NerouN26@users.noreply.github.com>
2023-04-15 19:03:41 +0300

    Finished version of the bot

--- Python/Telebots/Internet_cafe_bot.py
+++ Python/Telebots/Internet_cafe_bot.py
@@ -1,15 +1,13 @@
 import telebot
-from telebot import types
+from telebot import types, TeleBot
 import webbrowser
 
 bot = telebot.TeleBot('TOKEN')
 
-date = []
-j = 5
 @bot.message_handler(content_types=['text'])
 def start_reservation(message):
     if message.text == "/start":
-        bot.send_message(message.from_user.id, "Hello, this is a telegram bot of the Internet cafe 'nekita', here you can leave a reservation for the date, time, place you need. You can also request a number by which you can contact the manager of the cafe or ask a question right here, you will receive a response from the telegram account of one of our employees.")
+        bot.send_photo(message.from_user.id, 'http://i2.wp.com/www.fisharcadesgames.com/wp-content/uploads/2019/12/222538z4g0nrncqzeve34e.jpg', caption="Hello, this is a telegram bot of the Internet cafe 'nekita', here you can leave a reservation for the date, time, place you need. You can also request a number by which you can contact the manager of the cafe or ask a question right here, you will receive a response from the telegram account of one of our employees.")
         keyboard = types.InlineKeyboardMarkup()
         key_reservation = types.InlineKeyboardButton(text='Зарезервировать место | reservation', callback_data='reservation')
         keyboard.add(key_reservation)
@@ -17,70 +15,30 @@ def start_reservation(message):
         keyboard.add(key_number)
         key_Question = types.InlineKeyboardButton(text='Задать вопрос | Ask a Question', callback_data='Question')
         keyboard.add(key_Question)
-        question = "Hmmm~"
+        question = "What do you want to do?"
         bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
-
-
 @bot.callback_query_handler(func=lambda call: True)
-def processing_requests_from_buttons(call, message):
+def processing_requests_from_buttons(call):
+    global po, computer, name, second_name
     if call.data == "reservation":
-        reservation(message)
-    if call.data == "number":
+        bot.send_message(call.message.chat.id, "Please, select date according to this pattern: '12 April'")
+        bot.register_next_step_handler(call.message, reservation)
+    elif call.data == "number":
         bot.send_message(call.message.chat.id, "+79015685698")
-    if call.data == "Question":
-        Questions(message)
-
-
-@bot.message_handler(content_types=['text', 'photo'])
-def reservation(message):
-    global date, clock
-    bot.send_message(message.from_user.id, "Please, select date according to this pattern: '12 April'")
-    if message.text != 0:
-        date = message.text
-        bot.send_message(message.from_user.id, "Thank you, now tell me the time you want to come, for example '12:10'")
-        if message.text != date:
-            clock = message.text
-            bot.send_message(message.from_user.id, "Okay, now select the armored computer")
-            keyboard = types.InlineKeyboardMarkup()
-            key_1 = types.InlineKeyboardButton(text='1', callback_data='1')
-            keyboard.add(key_1)
-            key_2 = types.InlineKeyboardButton(text='2', callback_data='2')
-            keyboard.add(key_2)
-            key_3 = types.InlineKeyboardButton(text='3', callback_data='3')
-            keyboard.add(key_3)
-            key_4 = types.InlineKeyboardButton(text='4', callback_data='4')
-            keyboard.add(key_4)
-            key_5 = types.InlineKeyboardButton(text='5', callback_data='5')
-            keyboard.add(key_5)
-            key_6 = types.InlineKeyboardButton(text='6', callback_data='6')
-            keyboard.add(key_6)
-            key_7 = types.InlineKeyboardButton(text='7', callback_data='7')
-            keyboard.add(key_7)
-            key_8 = types.InlineKeyboardButton(text='8', callback_data='8')
-            keyboard.add(key_8)
-            key_9 = types.InlineKeyboardButton(text='9', callback_data='9')
-            keyboard.add(key_9)
-            key_10 = types.InlineKeyboardButton(text='10', callback_data='10')
-            keyboard.add(key_10)
-            question = "Hmmm~"
-            bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-            processing_computers_from_buttons(message)
-
-@bot.callback_query_handler(func=lambda call: True)
-def processing_computers_from_buttons(call, message):
-    global computer
-    if call.data == "1":
+    elif call.data == "Question":
+        bot.send_message(call.message.chat.id, "Ask a question, I will send it to our employee")
+        bot.register_next_step_handler(call.message, Questions1)
+    elif call.data == "1":
         computer = 1
         keyboard = types.InlineKeyboardMarkup()
         key_yes = types.InlineKeyboardButton(text='Да | Yes', callback_data='yes')
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 1")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 1")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "2":
         computer = 2
         keyboard = types.InlineKeyboardMarkup()
@@ -88,10 +46,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 2")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 2")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "3":
         computer = 3
         keyboard = types.InlineKeyboardMarkup()
@@ -99,10 +56,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 3")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 3")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "4":
         computer = 4
         keyboard = types.InlineKeyboardMarkup()
@@ -110,10 +66,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 4")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 4")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "5":
         computer = 5
         keyboard = types.InlineKeyboardMarkup()
@@ -121,10 +76,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 5")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 5")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "6":
         computer = 6
         keyboard = types.InlineKeyboardMarkup()
@@ -132,10 +86,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 6")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 6")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "7":
         computer = 7
         keyboard = types.InlineKeyboardMarkup()
@@ -143,10 +96,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 7")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 7")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "8":
         computer = 8
         keyboard = types.InlineKeyboardMarkup()
@@ -154,10 +106,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 8")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 8")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "9":
         computer = 9
         keyboard = types.InlineKeyboardMarkup()
@@ -165,10 +116,9 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 9")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 9")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
     elif call.data == "10":
         computer = 10
         keyboard = types.InlineKeyboardMarkup()
@@ -176,35 +126,80 @@ def processing_computers_from_buttons(call, message):
         keyboard.add(key_yes)
         key_no = types.InlineKeyboardButton(text='Нет | No', callback_data='no')
         keyboard.add(key_no)
-        bot.send_message(message.from_user.id, f"would you like to book a seat at {date}, at {clock}, computer 10")
+        bot.send_message(call.message.chat.id, f"would you like to book a seat at {date}, at {clock}, computer 10")
         question = "That's right?"
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        processing_processing_computers_from_buttons_from_buttons(message)
-
-@bot.callback_query_handler(func=lambda call: True)
-@bot.message_handler(content_types=['text', 'photo'])
-def processing_processing_computers_from_buttons_from_buttons(call, message):
-    global name, second_name
-    if call.data == "Yes":
-        bot.send_message(message.from_user.id, "what is your name?")
-        if message.text != clock:
-            name = message.text
-            bot.send_message(message.from_user.id, "What is your second name?")
-            if message.text != name:
-                second_name = message.text
-                iid = message.from_user.id
-                bot.send_message(1370983934, f"Computer reservation request: {date}, {clock}, {computer} computer from {name} {second_name}. User id: {iid}")
-                bot.send_message(message.from_user.id, "Succes!")
-                start_reservation(message)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
+    elif call.data == "yes":
+        bot.send_message(call.message.chat.id, "what is your name?")
+        bot.register_next_step_handler(call.message, yes)
     elif call.data == "no":
-        reservation(message)
+        bot.send_message(call.message.chat.id, "Try again: /start")
+
+@bot.message_handler(content_types=['text', 'photo'])
+def yes(message):
+    global po, computer, name, second_name
+    name = message.text
+    bot.send_message(message.from_user.id, "What is your second name?")
+    bot.register_next_step_handler(message, yes1)
+
+@bot.message_handler(content_types=['text', 'photo'])
+def yes1(message):
+    global po, computer, name, second_name, date, clock, iid
+    second_name = message.text
+    iid = message.from_user.id
+    bot.send_photo(1370983934, 'https://cdn.discordapp.com/attachments/801426522623836182/1096821260283035708/photo1681572735.jpeg', caption=f"Computer reservation request: {date}, {clock}, {computer} computer from {name} {second_name}. User id: {iid}")
+    bot.send_message(message.from_user.id, "Succes!")
+    bot.send_message(message.from_user.id, "Press: /start")
+    pass
+
+@bot.message_handler(content_types=['text', 'photo'])
+def reservation(message):
+    global date, clock
+    date = message.text
+    bot.send_message(message.from_user.id, "Thank you, now tell me the time you want to come, for example '12:10'")
+    bot.register_next_step_handler(message, reservation_1)
+
+@bot.message_handler(content_types=['text', 'photo'])
+def reservation_1(message):
+    global date, clock
+    clock = message.text
+    bot.send_message(message.from_user.id, "Okay, now select the armored computer")
+    keyboard = types.InlineKeyboardMarkup()
+    key_1 = types.InlineKeyboardButton(text='1', callback_data='1')
+    keyboard.add(key_1)
+    key_2 = types.InlineKeyboardButton(text='2', callback_data='2')
+    keyboard.add(key_2)
+    key_3 = types.InlineKeyboardButton(text='3', callback_data='3')
+    keyboard.add(key_3)
+    key_4 = types.InlineKeyboardButton(text='4', callback_data='4')
+    keyboard.add(key_4)
+    key_5 = types.InlineKeyboardButton(text='5', callback_data='5')
+    keyboard.add(key_5)
+    key_6 = types.InlineKeyboardButton(text='6', callback_data='6')
+    keyboard.add(key_6)
+    key_7 = types.InlineKeyboardButton(text='7', callback_data='7')
+    keyboard.add(key_7)
+    key_8 = types.InlineKeyboardButton(text='8', callback_data='8')
+    keyboard.add(key_8)
+    key_9 = types.InlineKeyboardButton(text='9', callback_data='9')
+    keyboard.add(key_9)
+    key_10 = types.InlineKeyboardButton(text='10', callback_data='10')
+    keyboard.add(key_10)
+    question = "Select computer number"
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text', 'photo'])
 def Questions(message):
     bot.send_message(message.from_user.id, "Ask a question, I will send it to our employee")
-    if message.text != second_name:
-        question = message.text
-        bot.send_message(1370983934, question)
-        bot.send_message(message.from_user.id, "Your question has been delivered")
-        start_reservation(message)
+    bot.register_next_step_handler(message, Questions1)
+
+@bot.message_handler(content_types=['text', 'photo'])
+def Questions1(message):
+    global questiono, iid
+    questiono = message.text
+    iid = message.from_user.id
+    bot.send_photo(1370983934, 'https://cdn.discordapp.com/attachments/801426522623836182/1096821259964264628/photo1681572720.jpeg', caption=f"Received a question from a user: {questiono}; User id: {iid}")
+    bot.send_message(message.from_user.id, "Your question has been delivered")
+    bot.send_message(message.from_user.id, "Press: /start")
+    pass
 bot.polling(none_stop=True, interval=0)
