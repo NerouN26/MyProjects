ba6d215644a805ba1a8d1763a9b9f17de3df8b7c
Nikita Bakulin <122795813+NerouN26@users.noreply.github.com>
2023-04-29 21:36:37 +0300

    End of work on the Internet_cafe database

---
+++ SQL/Internet_cafe/Create table internet_cafe_PC.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE internet_cafe.internet_cafe_pc (
+  Code int NOT NULL AUTO_INCREMENT,
+  Server_Name char(200) NOT NULL DEFAULT '',
+  CPU char(200) NOT NULL DEFAULT '',
+  Main_Memory_Gb int NOT NULL DEFAULT 500,
+  RAM_Gb int NOT NULL DEFAULT 8,
+  GPU char(200) NOT NULL DEFAULT '',
+  Status enum ('def', 'pro', 'vip') DEFAULT 'def',
+  Users int NOT NULL DEFAULT 0,
+  PRIMARY KEY (Code)
+)
+
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
---
+++ SQL/Internet_cafe/Create table internet_cafe_client.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE internet_cafe.internet_cafe_client (
+  ID int NOT NULL AUTO_INCREMENT,
+  Name text NOT NULL,
+  Email text NOT NULL,
+  Password text NOT NULL,
+  Key_word text NOT NULL,
+  Code_Comp int NOT NULL,
+  PRIMARY KEY (ID)
+)
+
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
